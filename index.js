b6421c876e375dc755ecac04a7423130d1ecf1bc
charlotte-divine <divinemaina@gmail.com>
2022-05-23 14:26:15 +0100

    create the page dynamically

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ div.innerHTML = `<div>
 <div>
 <p>${books.title}</p>
 <p>${books.Author}</p>
-<button type= "button">Remove</button>
+<button onclick="removeItem()" type= "button">Remove</button>
+<ul id = "dynamic">
+<li></li>
+<li>
+<ol></ol>
+</li>
+</ul>
 <hr>
 </div>
 <div>
@@ -25,3 +31,21 @@ div.innerHTML = `<div>
 `;
 collection.appendChild(div);
 });
+function addItem(){
+var ul = document.getElementById("dynamic");
+var title = document.getElementById("title");
+var author = document.getElementById("tuthor");
+var li = document.createElement("li");
+li.setAttribute('id',title.value);
+li.setAttribute('id',author.value);
+
+li.appendChild(document.createTextNode(title.value));'\n'
+li.appendChild(document.createTextNode(author.value));
+ul.append(li);
+}
+function removeItem(){
+    var ul = document.getElementById("dynamic");
+    var title = document.getElementById("title");
+   var li = document.createTextNode(title.value);
+   ul.removeItem(li);
+}
