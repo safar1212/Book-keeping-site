206a4955b734e0d405d785c29fa40ecce0ffc303
charlotte-divine <divinemaina@gmail.com>
2022-05-25 09:16:17 +0100

    fixe linters

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// book class: represent a book
-
-// const { list } = require("postcss");
-
-// const { List } = require("css-tree");
-
-class book {
+class Book {
   constructor() {
     this.books = JSON.parse(localStorage.getItem('Bookstore')) || [];
   }
@@ -48,113 +42,15 @@ class book {
   }
 }
 
-const Bookstore = new book();
-
-// Ul class: handles UI task
-
-class userInterface {
-  static displayBooks() {
-    const storedBooks = [
-      { title: 'Book 1', author: 'one' },
-      { title: 'Book 2', author: 'two' },
-    ];
-
-    // const books = displayBooks;
-
-    // books.forEach((book) => userInterface.addBookToList(book));
-
-    storedBooks.forEach((book) => userInterface.addBookToList(book));
-  }
-}
-
-// store class: store the task
-
-// event: display books
+const Bookstore = new Book();
 
 window.onload = () => { Bookstore.addBookToList(); };
 
-// events: add books
-
-// const addBook = document.querySelector("#book-form");
-
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#tuthor').value;
-  Bookstore.addBook(title, author);
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#tuthor');
+  Bookstore.addBook(title.value, author.value);
+  title.value = '';
+  author.value = '';
 });
-
-// events: remove books
-
-// let books = [];
-
-// const collection = document.getElementById('bookss');
-
-// function createForm() {
-//   const div = document.createElement('div');
-//   div.className = 'main-container';
-//   div.innerHTML = `
-// <div>
-// <div id = "dynamic">
-// </div>
-// </div>
-// <div>
-//   <form id = "bookForm">
-//     <input type="text" name = "title" id="title" required placeholder="Title"><br><br>
-//     <input type="text" name = "author" id="tuthor" required placeholder="Author"><br><br>
-//     <button id="addItem" type= "button">add</button>
-//   </form>
-// </div>
-// `;
-//   collection.appendChild(div);
-// }
-// createForm();
-
-// function dataStore() {
-//   localStorage.setItem('bookData', JSON.stringify(books));
-// }
-
-// function deleteBook(event) {
-//   const bookIndex = event.target.id;
-//   books.splice(bookIndex, 1);
-//   dataStore();
-//   displayBooks();
-// }
-
-// function displayBooks() {
-//   let listOfBooks = '';
-//   books.forEach((book, index) => {
-//     listOfBooks += ` <div class="book">
-//     <div>${book.title}</div>
-//     <div>${book.author}</div>
-//     <div>
-//         <button class='btn-delete' id=${index}>Remove</button>
-//     </div>
-//     <hr/>
-//   </div>`;
-//   });
-//   const div = document.getElementById('dynamic');
-//   div.innerHTML = listOfBooks;
-//   if (books.length > 0) {
-//     const deleteButtons = document.querySelectorAll('.btn-delete');
-//     deleteButtons.forEach((button) => { button.addEventListener('click', deleteBook); });
-//   }
-// }
-// function addItem() {
-//   const title = document.getElementById('title').value;
-//   const author = document.getElementById('tuthor').value;
-//   const form = document.forms[0];
-
-//   books.push({ title, author });
-//   dataStore();
-//   displayBooks();
-//   form.reset();
-// }
-
-// document.getElementById('addItem').addEventListener('click', addItem);
-// window.onload = () => {
-//   books = JSON.parse(localStorage.getItem('bookData')) || [];
-//   if (books) {
-//     displayBooks();
-//   }
-// };
